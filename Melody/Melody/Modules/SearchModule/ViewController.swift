dd06cb64d80ecfa4045a2f1a51ce38d79d259a50
Mikhail Ulanov <mixaulanoff@yandex.ru>
2024-04-11 10:55:55 +0300

    create app

---
+++ Melody/Melody/Modules/SearchModule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Melody
+//
+//  Created by Mike Ulanov on 11.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
